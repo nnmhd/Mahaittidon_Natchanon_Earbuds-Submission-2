ffca55a4c4c54405c94905e1f20545de97cffcec
nnmhd <natchanone@gmail.com>
2025-04-04 11:32:40 -0400

    CSS updated
    here, CSS has been updated

---
+++ js/revealText.js
@@ -0,0 +1,32 @@
+export function revealText() {
+  gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+  console.log("revealText");
+  const splitType = document.querySelectorAll(".reveal-text");
+  splitType.forEach((char, i) => {
+    const text = new SplitType(char, { types: "words" });
+    gsap.from(text.words, {
+      opacity: 0,
+      duration: 5,
+      stagger: 0.1,
+      scrub: false,
+      scrollTrigger: {
+        trigger: char,
+        start: "top 80%",
+        end: "top 20%",
+        markers: false,
+      },
+    });
+  });
+  const lenis = new Lenis();
+  lenis.on("scroll", (e) => {
+    console.log(e);
+  });
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+}
